b9b3a7e7456007212bb0d4ef6d43c955e947aaa4
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-03 20:03:56 +0700

    first commit

---
+++ coba.php
@@ -0,0 +1,17 @@
+<?php
+class coba
+{
+    public function __construct()
+    {
+        echo "ini adalah class coba";
+    }
+}
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
