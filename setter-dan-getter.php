2df3b27585481d81e71acabbcc6840a21657f0c4
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-04 19:19:37 +0700

    da

--- setter-dan-getter.php
+++ setter-dan-getter.php
@@ -1,11 +1,11 @@
 <?php
 class Produk
 {
-    public $judul,
+    private $judul,
         $penulis,
-        $penerbit;
-    protected $diskon = 0;
-    private $harga;
+        $penerbit,
+        $harga,
+        $diskon = 0;
 
     public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
     {
@@ -15,6 +15,51 @@ class Produk
         $this->harga = $harga;
         // echo "Hello World!";
     }
+
+    public function setjudul($judul)
+    {
+        // if (!is_string($judul)) {
+        //     throw new Exception("Judul Harus String");
+        // }
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+    public function setHarga($harga)
+    {
+        $this->harga = $harga;
+    }
+
 
 
     public function getHarga()
@@ -65,10 +110,7 @@ class Game extends Produk
     // {
     //     return $this->harga;
     // }
-    public function setDiskon($diskon)
-    {
-        $this->diskon = $diskon;
-    }
+
     public function getInfoProduk()
     {
         $str = "Game :  " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
@@ -100,3 +142,6 @@ echo "<hr>";
 
 $produk2->setDiskon(90);
 echo $produk2->getHarga();
+echo "<hr>";
+$produk1->setPenulis("Masashi Kishimoto");
+echo $produk1->getPenulis();
